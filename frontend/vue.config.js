8fd62bab5d001bdeb9e114f8e939b2601bf46d6b
wazatoki <wazatoki@gmail.com>
2022-11-27 01:23:47 +0000

    アプリのページタイトルをbrewing supportに変更する #8

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -5,4 +5,11 @@ module.exports = defineConfig({
   devServer: {
     port: 8080,
   },
+  pages: {
+    index: {
+      // entry for the page
+      entry: "src/main.ts",
+      title: "brewing support",
+    },
+  },
 });
