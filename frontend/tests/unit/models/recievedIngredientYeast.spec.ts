cdfe8365a8a2fe86385546acff34d3596f4d3d90
wazatoki <wazatoki@gmail.com>
2022-12-17 02:34:15 +0000

    recievedIngredientYeast test

---
+++ frontend/tests/unit/models/recievedIngredientYeast.spec.ts
@@ -0,0 +1,47 @@
+import * as recievedIngredientYeast from "@/models/recievedIngredientYeast";
+import * as ingredientYeast from "@/models/ingredientYeast";
+import { Unit } from "@/models/unit";
+import { createRecievedIngredientYeast } from "./helper";
+
+export const recievedIngredientYeasts: recievedIngredientYeast.RecievedIngredientYeast[] =
+  [];
+
+describe("RecievedIngredientYeast.ts", () => {
+  it("RecievedIngredientYeast shall create with no options.", () => {
+    const rIngredientYeast =
+      new recievedIngredientYeast.RecievedIngredientYeast();
+    expect(rIngredientYeast.id).toContain(recievedIngredientYeast.prefix);
+    expect(rIngredientYeast.quantity).toEqual(0);
+    expect(rIngredientYeast.yeast.id).toContain(ingredientYeast.prefix);
+    expect(rIngredientYeast.yeast.name).toEqual("");
+    expect(rIngredientYeast.yeast.attenuation).toEqual(0);
+    expect(rIngredientYeast.yeast.brewingUnit).toBeInstanceOf(Unit);
+    expect(rIngredientYeast.yeast.recievingUnit).toBeInstanceOf(Unit);
+    expect(rIngredientYeast.yeast.stockingUnit).toBeInstanceOf(Unit);
+  });
+
+  it("RecievedIngredientYeast shall create with options.", () => {
+    const yeasts = createRecievedIngredientYeast();
+    expect(yeasts[1].id).toContain("test-recieved_ingredient_yeast-id-1");
+    expect(yeasts[1].quantity).toEqual(1);
+    expect(yeasts[1].yeast.id).toEqual("test-yeast-id-1");
+    expect(yeasts[1].yeast.name).toEqual("test-yeast-name-1");
+    expect(yeasts[1].yeast.attenuation).toEqual(1);
+  });
+
+  it("convertToBaseUnit", () => {
+    const yeasts = createRecievedIngredientYeast();
+    jest.spyOn(yeasts[1].yeast.brewingUnit, "convertToBaseUnit");
+    yeasts[1].convertToBaseUnit;
+    expect(yeasts[1].yeast.brewingUnit.convertToBaseUnit).toHaveBeenCalled();
+  });
+
+  it("convertToStockingUnit", () => {
+    const yeasts = createRecievedIngredientYeast();
+    const result = yeasts[1].convertToStockingUnit;
+    expect(result).toEqual({
+      quantity: 1,
+      stockingUnit: yeasts[1].yeast.stockingUnit,
+    });
+  });
+});
